f303e743354e45718836778f51672cdf2511e411
yue <MoophyAlan@gmail.com>
2015-08-12 18:45:40 +1200

    modified:   JustTest.js

--- JustTest.js
+++ JustTest.js
@@ -1,8 +1,19 @@
 /**
  * Created by yue on 12/08/15.
  */
-var empty = {};
-console.log(empty.toString);
-// → function toString(){…}
-console.log(empty.toString());
-// → [object Object]
+
+//Ctor example in Js
+function Rabbit(type) {
+    this.type = type;
+}
+
+var killerRabbit = new Rabbit("killer");
+var blackRabbit = new Rabbit("black");
+
+console.log(blackRabbit.type);
+
+Rabbit.prototype.speak = function (line) {
+    console.log("The " + this.type + " rabbit says '" + line + "'");
+}
+blackRabbit.speak("Doom..");
+killerRabbit.speak("I'm a killer");
