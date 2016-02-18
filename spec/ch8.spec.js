65f4d77dd2d3348379095662ba8c1e8377ac27e4
moophy <MoophyAlan@gmail.com>
2016-02-18 23:51:20 +1300

    ch8 done

--- spec/ch8.spec.js
+++ spec/ch8.spec.js
@@ -16,4 +16,16 @@ describe('ch8',function(){
     };
     o.m();
   });
+
+  it('higher order functions', function(){
+    function not(f){
+      return function(){
+        return !f.apply(this, arguments);
+      };
+    }
+    var even = function(x){ return x % 2 === 0; };
+    var odd = not(even);
+
+    expect([1,3,5].every(odd)).toBe(true);
+  });
 });
